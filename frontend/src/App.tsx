686f293ac3724c3cba467e3d716a4c76756d6c93
fares <96792115+FaresAtef1@users.noreply.github.com>
2024-12-07 00:34:19 +0200

    Users Page2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './views/HomePage.tsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage.tsx";
 // import StadiumsView from './views/StadiumsView.tsx'
-import StadiumsView from './views/StadiumsView.tsx';
-
+import StadiumsView from "./views/StadiumsView.tsx";
+import Authorize from "./views/authorize.tsx";
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/stadiums" element={<StadiumsView />} />
+        <Route path="/authorize" element={<Authorize />} />
       </Routes>
     </Router>
   );
